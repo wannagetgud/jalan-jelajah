c1ae84fbe044c19fd45bd313730f6781743f4f7d
clstyn <fiorenzacelestyn@gmail.com>
2024-02-27 23:38:45 +0700

    fix register

--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -1,74 +1,39 @@
-import Button from "@/app/components/common/button";
+"use client";
+
 import Link from "next/link";
+import Button from "@/app/components/common/button";
+import InputBar from "@/app/components/common/inputBar";
 
 export default function Register() {
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-image-login font-poppins text-textwhite">
+      <div className="min-h-screen flex flex-col items-center justify-center bg-image-login font-poppins text-c-textwhite">
         <h1 className="text-4xl font-semibold">Daftar Sekarang</h1>
-        <div className="bg-textwhite rounded-[20px] text-textblack justify-center px-9 py-6 mt-6 w-[552px]">
+        <div className="bg-c-textwhite rounded-[20px] text-c-textblack justify-center px-9 py-6 mt-6 w-[552px]">
           <form action="" className="">
-            <div className="flex flex-col items-start w-7/8 mt-6">
-              <p className="font-medium text-xl">Email</p>
-              <input
-                className="w-full text-xl
-                                text-slate-500 rounded-[20px]
-                                focus:outline-none focus:ring-0 mt-2 p-6"
-                type="email"
-                name="email"
-                // value={formData.email}
-                // onChange={handleChange}
-                placeholder="Masukkan email"
-                required
-              />
-            </div>
+            <InputBar
+              inputName="email"
+              labelText="Email"
+              placeholderText="Masukkan email"
+            />
 
-            <div className="flex flex-col items-start w-7/8 mt-6">
-              <p className="font-medium text-xl">Username</p>
-              <input
-                className="w-full text-xl
-                                text-slate-500 rounded-[20px]
-                                focus:outline-none focus:ring-0 mt-2 p-6"
-                type="text"
-                name="username"
-                // value={formData.username}
-                // onChange={handleChange}
-                placeholder="Masukkan username"
-                required
-              />
-            </div>
+            <InputBar
+              inputName="username"
+              labelText="Username"
+              placeholderText="Masukkan username"
+            />
 
-            <div className="flex flex-col items-start w-7/8 mt-6">
-              <p className="font-medium text-xl">Nama</p>
-              <input
-                className="w-full text-xl
-                                text-slate-500 rounded-[20px]
-                                focus:outline-none focus:ring-0 mt-2 p-6"
-                type="text"
-                name="name"
-                // value={formData.name}
-                // onChange={handleChange}
-                placeholder="Masukkan nama"
-                required
-              />
-            </div>
+            <InputBar
+              inputName="name"
+              labelText="Nama"
+              placeholderText="Masukkan nama"
+            />
 
-            <div className="flex flex-col items-start w-7/8 mt-6">
-              <div className="flex justify-between items-end w-full">
-                <p className="font-medium text-xl">Password</p>
-              </div>
-              <input
-                className="w-full text-xl
-                                text-slate-500 rounded-[20px]
-                                focus:outline-none focus:ring-0 mt-2 p-6"
-                type="password"
-                name="password"
-                // value={formData.password}
-                // onChange={handleChange}
-                placeholder="Masukkan password"
-                required
-              />
-            </div>
+            <InputBar
+              inputName="password"
+              labelText="Password"
+              placeholderText="Masukkan password"
+            />
 
             <div className="flex flex-col items-center mt-8">
               <Button>DAFTAR</Button>
