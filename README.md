3edd77021106b64ac0399585353263329e1b29f6
clstyn <fiorenzacelestyn@gmail.com>
2024-02-27 22:22:19 +0700

    migrate to nextjs

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# DestiMatch
-Senior Project
-
-Kelompok Ongoing Capstone  
-Ketua Kelompok:  Ariq Tsaqif Aryasuta - 20/460541/TK/51130  
-Anggota 1:  Fiorenza Celestyn - 20/460545/TK/51134  
-Anggota 2:  Pramudya Kusuma Hardika - 20/460558/TK/51147
+FICPACT CUP 2024
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      screens: {
-        'fold': '280px'
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily: {
-        'poppins': ['Poppins', 'sans-serif']
-      },
-      fontSize: {
-        '2xs': '9px'
-      },
-      colors: {
-        "textwhite": "#F6F6F6",
-        "textblack": "#272D2F",
-        "orange": "#FF7143",
-        "white2": "#FAFAFA",
-        "pink1": "#FD3163",
-        "pink2": "#EF2E5F",
-        "blue": "#0D92EB"
-      },
-      boxShadow: {
-        "card": "3px 4px 10px 4px rgba(0, 0, 0, 0.1)",
-        "home": "6px 6px 20px 2px rgba(0, 0, 0, 0.1);"
-      }
     },
   },
   plugins: [],
-}
+};
