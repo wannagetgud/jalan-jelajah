deebbd789a698767679911c25f8bede1abf7ed05
clstyn <fiorenzacelestyn@gmail.com>
2024-03-02 20:48:39 +0700

    responsive about page

--- src/app/(routes)/(with-navbar)/about/page.jsx
+++ src/app/(routes)/(with-navbar)/about/page.jsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { ChevronDownCircle } from "lucide-react";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
 
 export default function About() {
   const cardContent = [
@@ -29,24 +31,66 @@ export default function About() {
     const element = document.getElementById("description");
     element.scrollIntoView({ behavior: "smooth" });
   };
+
+  const [sliderRef] = useKeenSlider(
+    {
+      slides: {
+        perView: 1,
+        spacing: 15,
+        loop: true,
+      },
+    },
+    [
+      (slider) => {
+        let timeout;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 2000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      },
+    ]
+  );
+
   return (
-    <>
+    <div className="overflow-hidden">
       <div className="flex container mx-auto h-[90vh] justify-center">
-        <div className="w-[544px] mr-[132px] flex flex-col justify-center ">
-          <p className="text-3xl font-medium">Tentang kami</p>
-          <p className="text-8xl font-bold mt-4">
+        <div className="w-full xl:w-[544px] xl:mr-[132px] flex flex-col justify-center gap-8 relative z-10">
+          <p className="text-lg md:text-xl xl:text-3xl font-medium">
+            Tentang kami
+          </p>
+          <p className="text-3xl md:text-5xl xl:text-8xl font-bold mt-4">
             Jalan
             <br />
             <span className="text-c-pink1">Jelajah</span>
           </p>
           <ChevronDownCircle
             color="black"
-            size={48}
-            className="animate-bounce mt-12 cursor-pointer"
+            className="animate-bounce mt-12 cursor-pointer w-8 h-8 md:w-12 md:h-12 "
             onClick={handleScroll}
           />
         </div>
-        <div className="flex justify-center items-center">
+        <div className="flex items-center max-md:absolute max-md:translate-x-36 max-md:translate-y-24">
           <Image
             src="/assets/hero2.png"
             width={500}
@@ -58,9 +102,9 @@ export default function About() {
 
       <div
         id="description"
-        className="bg-gradient-to-br from-c-pink2 to-c-orange-light w-full p-16 flex justify-center items-center gap-16"
+        className="bg-gradient-to-br from-c-pink2 to-c-orange-light w-full p-16 flex max-md:flex-col justify-center items-center gap-16"
       >
-        <div className="flex flex-col text-white text-2xl w-1/2">
+        <div className="flex flex-col text-white text-lg md:text-2xl md:w-1/2">
           <p>
             JalanJelajah bertujuan memberikan ide dan inspirasi tujuan
             perjalanan Anda dalam menjelajahi lima kota besar di Indonesia.
@@ -81,23 +125,38 @@ export default function About() {
       </div>
 
       <div className="flex flex-col container mx-auto h-screen items-center">
-        <h1 className="font-semibold text-4xl my-24">Keunggulan kami</h1>
-        <div className="grid grid-cols-3 gap-12 w-3/4">
+        <h1 className="font-semibold text-2xl md:text-4xl my-12 md:my-24">
+          Keunggulan kami
+        </h1>
+        <div className="grid grid-cols-3 gap-12 w-3/4 max-md:hidden">
           {cardContent.map((card) => (
             <Card key={card.id} {...card} />
           ))}
         </div>
+        <div className="md:hidden px-8">
+          <div className="keen-slider" ref={sliderRef}>
+            {cardContent.map((card) => (
+              <div key={card.id} className="keen-slider__slide">
+                <Card {...card} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
 const Card = ({ image, title, desc }) => {
   return (
-    <div className="rounded-2xl bg-white shadow-home p-10 flex flex-col gap-6 items-center">
-      <Image src={image} width={168} height={168} alt="card image" />
-      <h1 className="italic font-semibold text-c-pink1 text-2xl">{title}</h1>
-      <p className="text-center text-lg">{desc}</p>
+    <div className="rounded-2xl bg-white shadow-home p-4 md:p-10 flex flex-col gap-2 md:gap-6 items-center max-md:w-3/4 max-md:mx-auto">
+      <div className="flex items-center justify-center max-md:w-1/2">
+        <Image src={image} width={168} height={168} alt="card image" />
+      </div>
+      <h1 className="italic font-semibold text-c-pink1 text-lg md:text-2xl">
+        {title}
+      </h1>
+      <p className="text-center text-sm md:text-lg">{desc}</p>
     </div>
   );
 };
